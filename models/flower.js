c2b80ec4a362bb569377e6cb1ad40eccabce3449
Pkimura <paulo.kimura94@gmail.com>
2019-02-03 20:06:25 -0200

    Add Required flag to flower object

--- models/flower.js
+++ models/flower.js
@@ -2,11 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var FlowerSchema = new Schema({
-    name: String,
-    value: Number,
-    quantity: Number,
+    name:{ 
+        type: String,
+        required: true
+    },
+    value: {
+        type: Number,
+        required: true
+    },
+    quantity:{
+        type: Number,
+        required: true
+    },
    // picture:{data: Buffer, contentType: String },
-    description: String
+    description:{ 
+        type: String,
+        required: true
+    }
+
 });
 
 module.exports = mongoose.model('Flower', FlowerSchema);
