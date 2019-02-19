d52a12dfb9b7961cd7169276c48b150a6ce9249f
Pkimura <paulo.kimura94@gmail.com>
2019-02-19 15:19:06 -0300

    Server and database re-configuration

--- database.js
+++ database.js
@@ -2,12 +2,14 @@ var Mongoose = require('mongoose');
 
 
 // CARREGAR A DB
-Mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
-var db = Mongoose.connection;
+Mongoose.connect('mongodb://localhost:27017/mydb', {useNewUrlParser: true});
+const db = Mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function callback(){
-    console.log('Connection with database succeeded.');
-});
+db.on('error',(error) => {
+    console.log('Error connecting to MongoDB', error);
+  });
+  db.once('open', () => {
+    console.log('Connected to MongoDB');
+  });
 
 exports.db = db;
--- server.js
+++ server.js
@@ -1,58 +1,52 @@
-'use strict';
-const hapi = require('hapi');
-const db = require ('./database').db;
-const flowerRoutes = require('./routes/flowerRoute');
-const userRoutes = require ('./routes/userRoute');
-const user = require('./models/user');
-//const Bcrypt = require('mongoose-bcryp');
+const hapi = require("hapi");
+const { db } = require("./database");
+const flowerRoutes = require("./routes/flowerRoute");
+const userRoutes = require("./routes/userRoute");
+const user = require("./models/user");
+// const Bcrypt = require('mongoose-bcryp');
 
 const server = hapi.server({
-    port:3000,
-    host: '0.0.0.0'
+  port: 3000,
+  host: "0.0.0.0"
 });
-
 
 server.route({
-    method: 'GET',
-    path:'/api',
-    handler: (request, h) => {
-        return h.response({
-            'message': 'api'
-        });
-    } 
+  method: "GET",
+  path: "/api",
+  handler: (request, h) => {
+    return h.response({
+      message: "api"
+    });
+  }
 });
 
-flowerRoutes.forEach((route) =>{
-    server.route(route);
+flowerRoutes.forEach(route => {
+  server.route(route);
 });
-userRoutes.forEach((route) =>{
-    server.route(route);
+userRoutes.forEach(route => {
+  server.route(route);
 });
+/* eslint-disable */
+const validate = async (request, email, password, h) => { 
+  const User = User[email];
+  if (!user) {
+    return { credentials: null, isValid: false };
+  }
 
-const validate = async(request, email, password, h) => {
-    const User = User[email];
-        if (!user){
-            return {credentials: null, isValid: false};
-        }
-
-        const isValid = await Bcrypt.compare(password, user.password);
-        const credentials = {id: user.id, email: user.email};
-        return {isValid, credentials};
-
+  const isValid = await Bcrypt.compare(password, user.password);
+  const credentials = { id: user.id, email: user.email };
+  return { isValid, credentials };
 };
+/* eslint-enable */
+
 const init = async () => {
-
-    await server.start();
-    console.log(`Server funcionando no: ${server.info.uri}`);
-
+  await server.start();
+  console.log(`Server funcionando no: ${server.info.uri}`);
 };
 
-process.on('unhandledRejection', (err) =>{
-
-    console.log(err);
-    process.exit(1);
-
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
 });
 
 init();
-
