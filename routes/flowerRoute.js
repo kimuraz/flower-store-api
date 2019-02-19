37c79ad8e177986ef7270ed6f77142af873a7a1b
Pkimura <paulo.kimura94@gmail.com>
2019-02-19 15:18:16 -0300

    Enhancements on code

--- routes/flowerRoute.js
+++ routes/flowerRoute.js
@@ -1,55 +1,51 @@
-const Flower = require('../models/flower');
-
+const Flower = require("../models/flower");
 
 module.exports = [
-    {
-        method: ['GET','POST'],
-        path:'/api/flowers',
-        handler: (request, h) => {
-            switch(request.method){
-                case 'get':
-                    return Flower.find() // promisse -https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Promise
-                    break;
-                case 'post':
-                    const flower = new Flower({
-                        name: request.payload.name,
-                        price: request.payload.price,
-                        quantity: request.payload.quantity,
-                        description: request.payload.description
-                    });
-                   /* try{
+  {
+    method: ["GET", "POST"],
+    path: "/api/flowers",
+    handler: (request, h) => {
+      switch (request.method) {
+        case "get":
+          return Flower.find(); // promisse -https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Promise
+          break;
+        case "post":
+          const flower = new Flower({
+            name: request.payload.name,
+            price: request.payload.price,
+            quantity: request.payload.quantity,
+            description: request.payload.description
+          });
+          /* try{
                         flower.save()
                     } catch(err){
                         console.log(err);
                     } */
-                    return flower.save()
-                    break;
-                default: 
-                    return h.response({ code: 405 });
-            }
-        }
-    },
-
-    {
-        method:['PATCH', 'DELETE', 'GET'],
-        path:'/api/flowers/{id}',
-        handler: (request, h) => {
-            switch(request.method){
-                case 'delete':
-                    console.log('apagando');
-                    return Flower.deleteOne({'_id':request.params.id});//params da url
-                 
-                case 'patch':
-                    console.log ('alterando quantidade');
-                    return Flower.updateOne({'_quantity':request.param.quantity});
-                case 'get':
-                    console.log('')
-                    return Flower.findById({'_id': request.params.id});
-
-            }
-            return h.response(response);
-        }
-
+          return flower.save();
+          break;
+        default:
+          return h.response({ code: 405 });
+      }
     }
-    
+  },
+
+  {
+    method: ["PATCH", "DELETE", "GET"],
+    path: "/api/flowers/{id}",
+    handler: (request, h) => {
+      switch (request.method) {
+        case "delete":
+          console.log("apagando");
+          return Flower.deleteOne({ _id: request.params.id }); // params da url
+
+        case "patch":
+          console.log("alterando quantidade");
+          return Flower.updateOne({ _quantity: request.param.quantity });
+        case "get":
+          console.log("");
+          return Flower.findById({ _id: request.params.id });
+      }
+      return h.response(response);
+    }
+  }
 ];
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,86 +1,61 @@
-const User = require('../models/user');
-
+const User = require("../models/user");
 
 module.exports = [
-    {
-        method: ['GET','POST'],
-        path:'/api/user',
-        handler: (request, h) => {
-            switch(request.method){
-                case 'get':
-                    return User.find() // promisse -https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Promise
-                    break;
-                case 'post':
-                    const user = new User({
-                        name: request.payload.name,
-                        password: request.payload.password,
-                        email: request.payload.email,
-                        dateOfBirth: request.payload.dateOfBirth,
-                        socialNumber: request.payload.socialNumber,
-                        gender: request.payload.gender,
-                    });
-                   /* try{
-                        flower.save()
-                    } catch(err){
-                        console.log(err);
-                    } */
-                    return user.save()
-                    break;
-                default: 
-                    return h.response({ code: 405 });
-            }
-        }
-    },
+  {
+    method: ["GET", "POST"],
+    path: "/api/user",
+    handler: (request, h) => {
+      switch (request.method) {
+        case "get":
+          return User.find(); // promisse -https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Promise
+          break;
+        case "post":
+          const user = new User({
+            name: request.payload.name,
+            password: request.payload.password,
+            email: request.payload.email,
+            dateOfBirth: request.payload.dateOfBirth,
+            socialNumber: request.payload.socialNumber,
+            gender: request.payload.gender
+          });
 
-    {
-        method:['PATCH', 'DELETE', 'GET'],
-        path:'/api/user/{id}',
-        handler: (request, h) => {
-            switch(request.method){
-                case 'delete':
-                    console.log('apagando');
-                    return User.deleteOne({'_id':request.params.id});//params da url
-                 
-                case 'patch':
-                    return user.updateOne({'email':request.param.email});
-                case 'get':
-                    console.log('')
-                    return Flower.findById({'_id': request.params.id});
+          return user.save();
+          break;
+        default:
+          return h.response({ code: 405 });
+      }
+    }
+  },
 
-            }
-            return h.response(response);
-        }
+  {
+    method: ["PATCH", "DELETE", "GET"],
+    path: "/api/user/{id}",
+    handler: (request, h) => {
+      switch (request.method) {
+        case "delete":
+          return User.deleteOne({ _id: request.params.id }); // params da url
 
-    },
-    
-    {
-        method:['PATCH'],
-        path:'/api/user/email',
-        handler: (request, h) => {
-         
-               return user.updateOne({'email':request.param.email});
+        case "patch":
+          return user.updateOne(request.params);
+        case "get":
+          return user.findById({ _id: request.params.id });
+      }
+      return h.response(response);
+    }
+  },
 
-        }
-        },
-    
-{
-        method:['PATCH'],
-        path:'/api/user/adress',
-        handler: (request, h) => {
-         
-               return user.updateOne({'deliveryAdress':request.param.deliveryAdress});
+  {
+    method: ["POST"],
+    path: "/api/user/auth",
+    handler: (validate = async (request, email, password, h) => {
+      const User = User[email];
+      if (!user) {
+        return { credentials: null, isValid: false };
+      }
 
-        }
-        },
-        {
-            method:['PATCH'],
-            path:'/api/user/password',
-            handler: (request, h) => {
-             
-                   return user.updateOne({'password':request.param.password});
-    
-            }
-            }
-    ];
-
-    
+      const isValid = await Bcrypt.compare(password, user.password);
+      const credentials = { id: user.id, email: user.email };
+      return { isValid, credentials };
+    })
+  }
+];
